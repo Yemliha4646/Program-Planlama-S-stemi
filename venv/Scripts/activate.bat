ee993bb5bf9585b8c269e1658b61f902e460633a
karak <karakusyemliha46@gmail.com>
2026-05-23 06:51:20 +0300

    revize

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\karak\Desktop\Program Planlama Sİstemi\Program-Planlama-S-stemi\venv"
+set "VIRTUAL_ENV=C:\Users\karak\Desktop\Program-Planlama-S-1stemi\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
